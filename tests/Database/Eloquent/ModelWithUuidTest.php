1a8269fce80708f5c05dd5f409f043b6ec304533
Matt Inamdar <matt@goldspecdigital.com>
2019-06-30 11:42:23 +0100

    [1.1] Feature/UUID v1 support (#6)
    * Added support for UUIDv1
    
    * Updated README
    
    * Updated composer.json
    
    * Closes #3

--- tests/Database/Eloquent/ModelWithUuidTest.php
+++ tests/Database/Eloquent/ModelWithUuidTest.php
@@ -4,28 +4,51 @@ declare(strict_types=1);
 
 namespace GoldSpecDigital\LaravelEloquentUUID\Tests\Database\Eloquent;
 
-use GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid;
+use GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid1;
+use GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid4;
 use GoldSpecDigital\LaravelEloquentUUID\Tests\TestCase;
 use Illuminate\Support\Str;
 
 class ModelWithUuidTest extends TestCase
 {
     /** @test */
-    public function it_generates_a_uuid_when_the_id_has_not_been_set(): void
+    public function it_generates_a_uuid1_when_the_id_has_not_been_set(): void
     {
-        /** @var \GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid $testModel */
-        $testModel = TestModelWithUuid::query()->create();
+        /** @var \GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid1 $testModel */
+        $testModel = TestModelWithUuid1::query()->create();
 
         $this->assertEquals(36, mb_strlen($testModel->id));
     }
 
     /** @test */
-    public function it_uses_the_uuid_provided_when_id_has_been_set(): void
+    public function it_generates_a_uuid4_when_the_id_has_not_been_set(): void
+    {
+        /** @var \GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid4 $testModel */
+        $testModel = TestModelWithUuid4::query()->create();
+
+        $this->assertEquals(36, mb_strlen($testModel->id));
+    }
+
+    /** @test */
+    public function it_uses_the_uuid1_provided_when_id_has_been_set(): void
     {
         $uuid = Str::uuid()->toString();
 
-        /** @var \GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid $testModel */
-        $testModel = TestModelWithUuid::query()->create([
+        /** @var \GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid1 $testModel */
+        $testModel = TestModelWithUuid1::query()->create([
+            'id' => $uuid,
+        ]);
+
+        $this->assertEquals($uuid, $testModel->id);
+    }
+
+    /** @test */
+    public function it_uses_the_uuid4_provided_when_id_has_been_set(): void
+    {
+        $uuid = Str::uuid()->toString();
+
+        /** @var \GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithUuid4 $testModel */
+        $testModel = TestModelWithUuid4::query()->create([
             'id' => $uuid,
         ]);
 
--- tests/Database/Eloquent/ModelWithoutUuidTest.php
+++ tests/Database/Eloquent/ModelWithoutUuidTest.php
@@ -4,17 +4,26 @@ declare(strict_types=1);
 
 namespace GoldSpecDigital\LaravelEloquentUUID\Tests\Database\Eloquent;
 
-use GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithoutUuid;
+use GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithoutUuid1;
+use GoldSpecDigital\LaravelEloquentUUID\Tests\Models\TestModelWithoutUuid4;
 use GoldSpecDigital\LaravelEloquentUUID\Tests\TestCase;
 use PDOException;
 
 class ModelWithoutUuidTest extends TestCase
 {
     /** @test */
-    public function it_does_not_generate_a_uuid_when_no_id_has_been_set(): void
+    public function it_does_not_generate_a_uuid1_when_no_id_has_been_set(): void
     {
         $this->expectException(PDOException::class);
 
-        TestModelWithoutUuid::query()->create();
+        TestModelWithoutUuid1::query()->create();
+    }
+
+    /** @test */
+    public function it_does_not_generate_a_uuid4_when_no_id_has_been_set(): void
+    {
+        $this->expectException(PDOException::class);
+
+        TestModelWithoutUuid4::query()->create();
     }
 }
